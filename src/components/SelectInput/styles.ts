870ad7a29f845c81ae334c7dc0dd5e08c6d5bf56
Felipe Zeba <felipe_zeba@outlook.com>
2024-02-08 18:42:33 -0600

    feat: add SelectInput component

---
+++ src/components/SelectInput/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Select = styled.select`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: none;
+  border-radius: 5px;
+  background-color: #444;
+  color: #fff;
+  font-size: 1.2rem;
+`;
+
+export const Option = styled.option`
+  background-color: #444;
+  color: #ffcc00;
+  font-size: 1.2rem;
+`;
