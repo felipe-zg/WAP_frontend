83cb0df3586a96d9769065217c1b2aadd5f9f3ed
Felipe Zeba <felipe_zeba@outlook.com>
2024-02-08 18:48:12 -0600

    chore: create types

---
+++ src/types/errorHandlers.d.ts
@@ -0,0 +1,4 @@
+declare type ErrorHandlers = {
+  onSuccess?: (data: any) => void;
+  onError?: () => void;
+}
---
+++ src/types/game.d.ts
@@ -0,0 +1,13 @@
+declare type Game = {
+  id: string;
+  title: string;
+  description: string;
+  price: string;
+  rating: string;
+  image: string;
+  platform: string;
+  genre: string;
+  publisher: string;
+  releaseDate: string;
+  developer: string;
+}
