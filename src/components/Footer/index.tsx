85af6e7e355531f32d09e934c2d2b43c6df1d0f0
Felipe Zeba <felipe_zeba@outlook.com>
2024-02-08 18:40:53 -0600

    feat: add Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FooterContainer, FooterText } from "./styles";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterText>© 2024 - All rights reserved</FooterText>
+      <FooterText>Felipe Zeba</FooterText>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 60px 16px;
+  background-color: #333;
+`;
+
+export const FooterText = styled.p`
+  font-size: 14px;
+  margin: 0;
+  color: #ffcc00;
+  text-align: center;
+`;
